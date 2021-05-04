82d9e7971eeeb61721a0c14f30135a0591c697b3
zshzero <ashwinkumar3098@gmail.com>
2021-05-04 20:20:19 +0530

    Implemented Login & Logout

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Hello.Data.Entities;
+using Hello.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Hello.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> logger;
+        private readonly SignInManager<StoreUser> signInManager;
+
+        public AccountController(ILogger<AccountController> logger, SignInManager<StoreUser> signInManager)
+        {
+            this.logger = logger;
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            if(this.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index","App");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Username,
+                                                               model.Password,
+                                                               model.RememberMe,
+                                                               false);
+
+                if(result.Succeeded)
+                {
+                    if(Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        return Redirect(Request.Query["ReturnUrl"].First());
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index","App");
+                    }
+                }
+            }
+
+            ModelState.AddModelError("","Failed to Login");
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index","App");
+        }
+    }
+}
---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Hello.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
