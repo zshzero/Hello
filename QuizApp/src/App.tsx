fff6ad7fba307948ee10eb21382a550d67db2969
zshzero <ashwinkumar3098@gmail.com>
2021-10-23 15:52:32 +0530

    App Styling

--- QuizApp/src/App.tsx
+++ QuizApp/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { fetchQuizQuestions } from './API';
 import QuestionCard from './Components/QuestionCard';
 import { QuestionsState, Difficulty } from './API';
+import { GlobalStyle, Wrapper } from './App.styles';
 
 export type AnswerObject = {
   question: string;
@@ -66,35 +67,38 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>REACT QUIZ</h1>
-      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
-        <button className='start' onClick={startTrivia}>
-          Start
-        </button>
-      ) : null}
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <h1>REACT QUIZ</h1>
+        {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+          <button className='start' onClick={startTrivia}>
+            Start
+          </button>
+        ) : null}
 
-      {!gameOver ? <p className='score'>Score: {score}</p> : null}
+        {!gameOver ? <p className='score'>Score: {score}</p> : null}
 
-      {loading ? <p>Loading Questions...</p> : null}
+        {loading ? <p>Loading Questions...</p> : null}
 
-      {!loading && !gameOver && (
-        <QuestionCard
-          questionNr={number + 1}
-          totalQuestions={TOTAL_QUESTIONS}
-          question={questions[number].question}
-          answers={questions[number].answers}
-          userAnswer={userAnswers ? userAnswers[number] : undefined}
-          callback={checkAnswer}
-        />
-      )}
+        {!loading && !gameOver && (
+          <QuestionCard
+            questionNr={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
+        )}
 
-      {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
-        <button className='next' onClick={nextQuestion}>
-          Next Question
-        </button>
-      ) : null}
-    </div >
+        {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+          <button className='next' onClick={nextQuestion}>
+            Next Question
+          </button>
+        ) : null}
+      </Wrapper >
+    </>
   );
 }
 
--- QuizApp/src/Components/QuestionCard.tsx
+++ QuizApp/src/Components/QuestionCard.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { AnswerObject } from "../App";
+import { Wrapper } from "../App.styles";
+import { ButtonWrapper } from "./QuestionCard.styles";
 
 type Props = {
     question: string;
@@ -18,21 +20,25 @@ const QuestionCard: React.FC<Props> = ({
     questionNr,
     totalQuestions,
 }) => (
-    <div>
+    <Wrapper>
         <p className='number'>
             Question: {questionNr} / {totalQuestions}
         </p>
         <p dangerouslySetInnerHTML={{ __html: question }} />
         <div>
             {answers.map((answer) => (
-                <div key={answer}>
+                <ButtonWrapper
+                    key={answer}
+                    correct={userAnswer?.correctAnswer === answer}
+                    userClicked={userAnswer?.answer === answer}
+                >
                     <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
                         <span dangerouslySetInnerHTML={{ __html: answer }} />
                     </button>
-                </div>
+                </ButtonWrapper>
             ))}
         </div>
-    </div >
+    </Wrapper>
 )
 
 export default QuestionCard
