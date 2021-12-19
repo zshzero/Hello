c6645ac9fcc3a489d0128e76d2d4c5875f00881d
zshzero <ashwinkumar3098@gmail.com>
2021-12-19 09:39:46 +0530

    Building & Reusing Another Component Styling with CSS Classes Pros & Dynamic Content Handling Events Introducing State Event Props

--- meetups/src/App.js
+++ meetups/src/App.js
@@ -1,7 +1,22 @@
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import Todo from "./Components/Todo";
+
 function App() {
-  return (
-    <div>Hello World!!</div>
-  );
+	return (
+		<Container>
+			<h1>My Todos</h1>
+
+			<Row>
+				<Col><Todo header="Task 1" /></Col>
+				<Col><Todo header="Task 2" /></Col>
+				<Col><Todo header="Task 3" /></Col>
+			</Row>
+
+		</Container>
+	);
 }
 
 export default App;
