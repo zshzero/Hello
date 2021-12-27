b964b4ab21597d9c8ed32a122a0b9338c7cbfe67
zshzero <ashwinkumar3098@gmail.com>
2021-12-27 09:24:05 +0530

    Adding a Form     Getting User Input & Handling Form Submission     Preparing the App for HTTP     Sending a Post Request     Navigating Programmatically     Getting Started with Fetching Data     useEffect

--- meetups/src/Pages/AllMeetups.js
+++ meetups/src/Pages/AllMeetups.js
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+import { Spinner } from "react-bootstrap";
+
 import MeetupList from "../Components/meetups/MeetupList"
 
 const DUMMY_DATA = [
@@ -22,7 +25,46 @@ const DUMMY_DATA = [
 ];
 
 export default () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch(
+            'https://react-getting-started-48dec-default-rtdb.firebaseio.com/meetups.json'
+        )
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                const meetups = [];
+
+                for (const key in data) {
+                    const meetup = {
+                        id: key,
+                        ...data[key]
+                    };
+
+                    meetups.push(meetup);
+                }
+
+                setIsLoading(false);
+                setLoadedMeetups(meetups);
+            })
+    }, [])
+
+    if (isLoading) {
+        return (
+            <Spinner animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </Spinner>
+        )
+    }
+
     return (
-        <div><MeetupList meetups={DUMMY_DATA} /></div>
+        <section>
+            <h1>All Meetups</h1>
+            <MeetupList meetups={loadedMeetups} />
+        </section>
     )
 }
--- meetups/src/Pages/NewMeetup.js
+++ meetups/src/Pages/NewMeetup.js
@@ -1,5 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+
+import NewMeetupForm from '../Components/meetups/NewMeetupForm'
+
 export default () => {
+    const navigate = useNavigate();
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'https://react-getting-started-48dec-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(() => {
+            navigate('/', { replace: true })
+        })
+    }
+
     return (
-        <div>New Meetup Page</div>
+        <>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
+        </>
     )
 }
